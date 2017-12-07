32581495ca60c0db144e2e15f09242d4f3893b74
Sergey <sunliveua@gmail.com>
2017-12-07 12:40:09 +0200

    error handlings

--- server.js
+++ server.js
@@ -12,23 +12,42 @@ const app = new koa();
 //   ctx.body = { name: body.name.toUpperCase() };
 // });
 
-app.use(async ctx => {
-  ctx.status = 404;
+// app.use(async ctx => {
+//   ctx.status = 404;
 
-  switch (ctx.accepts('html', 'json')) {
-    case 'html':
-      ctx.type = 'html';
-      ctx.body = '<p>page not found</p>';
-      break;
-    case 'json':
-      ctx.body = {
-        message: 'Page not found',
-      };
-      break;
-    default:
-      ctx.type = 'text';
-      ctx.body = 'page not found';
+//   switch (ctx.accepts('html', 'json')) {
+//     case 'html':
+//       ctx.type = 'html';
+//       ctx.body = '<p>page not found</p>';
+//       break;
+//     case 'json':
+//       ctx.body = {
+//         message: 'Page not found',
+//       };
+//       break;
+//     default:
+//       ctx.type = 'text';
+//       ctx.body = 'page not found';
+//   }
+// });
+
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    ctx.status = err.status || 500;
+    ctx.type = 'html';
+    ctx.body = `<h1>Error: ${ctx.status}</h1>`;
+    ctx.app.emit('error', err, ctx);
   }
+});
+
+app.use(async () => {
+  throw new Error('Something bad happened');
+});
+
+app.on('error', err => {
+  console.log(err.message);
 });
 
 if (!module.parent) app.listen(8080);
