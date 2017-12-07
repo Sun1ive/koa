5fd25667507d570c4cfc815336f298cd9ff6d956
Sergey <sunliveua@gmail.com>
2017-12-07 12:30:26 +0200

    404

--- server.js
+++ server.js
@@ -2,14 +2,33 @@ const koa = require('koa');
 const koaBody = require('koa-body');
 const app = new koa();
 
-app.use(koaBody());
+// app.use(koaBody());
+
+// app.use(async ctx => {
+//   const body = ctx.request.body;
+
+//   if (!body.name) ctx.throw(400, 'Error there is nothing to do here!!');
+
+//   ctx.body = { name: body.name.toUpperCase() };
+// });
 
 app.use(async ctx => {
-  const body = ctx.request.body;
+  ctx.status = 404;
 
-  if (!body.name) ctx.throw(400, 'Error there is nothing to do here!!');
-
-  ctx.body = { name: body.name.toUpperCase() };
+  switch (ctx.accepts('html', 'json')) {
+    case 'html':
+      ctx.type = 'html';
+      ctx.body = '<p>page not found</p>';
+      break;
+    case 'json':
+      ctx.body = {
+        message: 'Page not found',
+      };
+      break;
+    default:
+      ctx.type = 'text';
+      ctx.body = 'page not found';
+  }
 });
 
 if (!module.parent) app.listen(8080);
