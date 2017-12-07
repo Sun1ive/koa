55045f2104ec153acb65aff57a196f6ead375034
Sergey <sunliveua@gmail.com>
2017-12-07 12:25:16 +0200

    added koabody

--- server.js
+++ server.js
@@ -1,22 +1,15 @@
 const koa = require('koa');
+const koaBody = require('koa-body');
 const app = new koa();
 
-app.use(async (ctx, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  console.log('ms1 =', ms);
-});
-
-app.use(async (ctx, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  console.log('ms2 =', ms);
-});
+app.use(koaBody());
 
 app.use(async ctx => {
-  ctx.body = 'Hello world!';
+  const body = ctx.request.body;
+
+  if (!body.name) ctx.throw(400, 'Error there is nothing to do here!!');
+
+  ctx.body = { name: body.name.toUpperCase() };
 });
 
 if (!module.parent) app.listen(8080);
