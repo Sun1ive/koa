a6796b2f5fa0599fc1c588d3687ce9e3d09fdbd1
Sergey <sunliveua@gmail.com>
2017-12-07 12:16:59 +0200

    async

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const koa = require('koa');
 const app = new koa();
 
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log('ms1 =', ms);
+});
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log('ms2 =', ms);
+});
+
 app.use(async ctx => {
   ctx.body = 'Hello world!';
 });
 
-if (!module.parent) app.listen(8080)
+if (!module.parent) app.listen(8080);
